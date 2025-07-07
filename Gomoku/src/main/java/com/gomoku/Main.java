bad4b6391b505fec6775897433b72b7884379442
Axente Andrei <axandrei922@gmail.com>
2025-07-07 10:31:50 +0300

    Delete Gomoku directory

--- Gomoku/src/main/java/com/gomoku/Main.java
+++
@@ -1,9 +0,0 @@
-package com.gomoku;
-
-import com.gomoku.view.StartMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new StartMenu());
-    }
-}
